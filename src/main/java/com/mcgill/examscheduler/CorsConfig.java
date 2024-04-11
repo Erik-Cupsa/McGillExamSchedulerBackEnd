d5a70037db7b6028377f50e79d94146272ed38f8
Erik Cupsa <86483911+Erik-Cupsa@users.noreply.github.com>
2024-04-11 11:50:43 -0400

    Add files via upload

--- src/main/java/com/mcgill/examscheduler/CorsConfig.java
+++ src/main/java/com/mcgill/examscheduler/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "https://mcgillscheduler.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "https://mcgillscheduler.vercel.app/")
                         .allowedMethods("GET");
             }
         };
