23ae84a1c02f85e71df8c2b89d5c86190406c048
Erik Cupsa <2003eec@gmail.com>
2024-05-23 13:02:42 -0400

    Created historic exam service, controller, model, and repository

--- src/main/java/com/mcgill/examscheduler/exam/service/HistoricExamService.java
+++ src/main/java/com/mcgill/examscheduler/exam/service/HistoricExamService.java
@@ -1,7 +1,35 @@
 package com.mcgill.examscheduler.exam.service;
 
+import com.mcgill.examscheduler.exam.model.HistoricExam;
+import com.mcgill.examscheduler.exam.repo.HistoricExamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class HistoricExamService {
+    private final HistoricExamRepository historicExamRepository;
+
+    @Autowired
+    public HistoricExamService(HistoricExamRepository historicExamRepository) {
+        this.historicExamRepository = historicExamRepository;
+    }
+
+    public List<HistoricExam> getExamsByNameAndYear(List<String> classNames, List<String> years) {
+        List<String> lowercaseHistoricExamNames = classNames.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        List<String> lowercaseYears = years.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+
+        // Filter exams by class names provided in the list
+        return historicExamRepository.findAll().stream()
+                .filter(historicExam -> lowercaseHistoricExamNames.contains(historicExam.getCourse().toLowerCase())
+                        && lowercaseYears.contains(historicExam.getYear().toLowerCase()))
+                .collect(Collectors.toList());
+    }
 }
